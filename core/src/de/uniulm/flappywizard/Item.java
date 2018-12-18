f432168d776c343f9cf2c3df9162415c62a22cad
depp <zimmer.thore@web.de>
2018-12-18 16:11:00 +0100

    item added

--- core/src/de/uniulm/flappywizard/Item.java
+++ core/src/de/uniulm/flappywizard/Item.java
@@ -7,12 +7,17 @@ import com.badlogic.gdx.math.Vector2;
 public class Item {
      private Vector2 position;
      public Texture texture;
+     public Texture texturer;
+     public Texture textureb;
      public Rectangle hitbox;
      private Vector2 size;
 
      public Item(int x,int y){
          position=new Vector2(x,y);
          texture=new Texture( "core/assets/png/item_blau.png");
+         textureb=new Texture("core/assets/png/item_blau.png");
+         texturer=new Texture("core/assets/png/item_rot.png");
+
 
          size=new Vector2(texture.getWidth()*0.125f,texture.getHeight()*0.125f);
          hitbox=new Rectangle(position.x,position.y,size.x,size.y);
@@ -26,9 +31,9 @@ public class Item {
         this.position = new Vector2(x,y);
     }
     public void turnRed(){
-         texture=new Texture("core/assets/png/item_rot.png");
+         texture=texturer;
     }
     public void turnBlue(){
-        texture=new Texture("core/assets/png/item_blau.png");
+        texture=textureb;
     }
 }
